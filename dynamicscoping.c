9bc98691c11510d6624781da67fbb2945a1c32b2
YeabkalW <wyeabkal@gmail.com>
2022-10-02 16:01:28 +0300

    learned basics of recuring fundtion

---
+++ dynamicscoping.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * here i am learning about dynamic scoping of function.
+ * 
+ * what is dynamic scoping 
+ * 
+ * in dynamic scoping, definition of a variable is resolved by searching its
+ * containing block and if not found, then searching its calling function and 
+ * if still not found then the function which is called that calling function
+ * will be searched and so on.
+ * 
+ * 
+ * 
+ */
---
+++ stat-dyna_scopingp4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Important takeaways 
+ * 
+ * in most of the programing languages, static scoping
+ * is followed instead of dynamic scoping 
+ * 
+ * languages, including Algol, Pascal, C, Haskell, Scheme etc.are 
+ * statically scoped.
+ * 
+ * Some languages, including SNOBOL, APL, Lisp etc are dynamically scoped
+ * 
+ * as C follows static scoping therefore it is not possible to see programmatically
+ * how dynamic scoping works in C.
+ * 
+ * 
+ */
